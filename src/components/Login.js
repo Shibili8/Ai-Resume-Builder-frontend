4fa97d12b5461c687206f9534a498ddab6c93023
Shibili8 <shibilishibizz99@gmail.com>
2026-03-28 12:37:30 +0530

    Protected Route added

--- src/components/Login.js
+++ src/components/Login.js
@@ -89,7 +89,7 @@ export default function Login() {
           <button
             type="button"
             onClick={() => setShowPassword(!showPassword)}
-            className="absolute right-3 top-3 text-gray-500 hover:text-gray-700 focus:outline-none"
+            className="absolute right-3 top-3 text-sm text-blue-600 bg-transparent hover:bg-transparent focus:outline-none select-none"
           >
             {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
           </button>
